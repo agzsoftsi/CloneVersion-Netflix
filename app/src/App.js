910de6fd62c41b7c7123e7a8c767a8ceea4f9947
Carlos Garcia <agzsoftsi@gmail.com>
2021-03-10 18:22:08 -0500

    Create rows for each category

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import requests from './request';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <Row title="NETFLIX ORIGINALS"  fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow={true}
+    />
+    <Row title="Trending Now"  fetchUrl={requests.fetchTrending}/>
+    <Row title="Top Rated"  fetchUrl={requests.fetchTopRated}/>
+    <Row title="Action Movies"  fetchUrl={requests.fetchActionMovies}/>
     </div>
   );
 }
